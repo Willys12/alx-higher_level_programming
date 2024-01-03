2b16f9de5484eca4a7fcf448ebf352d359069344
Willys12 <ben.willys92@gmail.com>
2024-01-03 11:24:32 -0800

    Return last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num_str = str(abs(number))
-    if num_str[-1] == '0':
-        return int(num_str)
+    if number < 0:
+        last_digit = number % 10
+        print(-(last_digit), end='')
     else:
-        return int(num_str[-1])
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit)
