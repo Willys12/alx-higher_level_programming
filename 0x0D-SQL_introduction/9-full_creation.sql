3bafa29951583d6920a5fd3ce3d9929d6d067397
Willys12 <ben.willys92@gmail.com>
2024-03-14 23:54:28 +0300

    Adding details

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates a new table with new items.
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+INSERT INTO `second_table`
+VALUES (1, "JOHN", 10),
+       (2, "ALEX", 3),
+       (3, "BOB", 14),
+       (4, "GEORGE", 8);
+
+SELECT * FROM `second_table`;
