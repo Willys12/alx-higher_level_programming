a04e2d6e893a16812b0711d98d941fe7d131cefd
Willys12 <ben.willys92@gmail.com>
2024-03-15 00:27:22 +0300

    Files added

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Displays the number of records.
-SELECT COUNT (*)
+SELECT COUNT(*)
 FROM  `first_table`
 WHERE `id` = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `second_table` (
     `score` INT
 );
 
-INSERT INTO `second_table` (1, "JOHN", 10),
-                           (2, "ALEX", 3),
-                           (3, "BOB", 14),
-                           (4, "GEORGE", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "JOHN", 10),
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3),
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "BOB", 14),
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "GEORGE", 8);
